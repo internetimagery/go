c945b1300e5e3e5bfeb71e89bbbaaec44a5d24a1
Jason <jason.dixon.email@gmail.com>
2016-03-19 01:08:56 +1300

    build structure

--- js/main.coffee
+++ js/main.coffee
@@ -1,92 +1,21 @@
 # Game Code
 
-# Parse out url parameters
-parseURL = ()->
-  url = window.location.href # Get current URL
-  elem = document.createElement("a") # Create temporary element
-  elem.href = url # Apply url for built in parsing
-  query = decodeURI(elem.hash || elem.search) # Pull out query
-  parse = {}
-  query.replace /([^?#=&]+)(=([^&]*))?/g, ($0, $1, $2, $3)->
-    parse[$1] = $3
-  return parse
-
-# Get bit positions from a number
-getPositions = (num)->
-  num.toString(2)
-
-# Generate number given positions
-genPositions = (pos)->
-  parseInt(pos, 2)
-
-# Build out a board given positions of stones
-formBoard = (size, white, black)->
-  full_size = size ** 2
-  board = Array() # Initialize board
-  row = Array() # First row
-  tmp = Array()
-  for i in [1 ... full_size + 1] # fill in the board backwards, reading right to left on the binary positions
-    white_pos = parseInt(white.charAt(white.length - i)) # Get stone positions
-    black_pos = parseInt(black.charAt(black.length - i))
-    if white_pos and black_pos # White and Black cannot occupy the same space
-      throw "Invalid Board Configuration"
-    else if white_pos # add board position
-      row.push("white")
-    else if black_pos
-      row.push("black")
-    else
-      row.push("")
-    if row.length == size
-      board.push(row)
-      row = Array()
-  return board
-
-# Parse a board into its positions
-parseBoard = (board)->
-  white = ""
-  black = ""
-  for row in board
-    for cell in row
-      white_pos = "0"
-      black_pos = "0"
-      if cell == "white"
-        white_pos = "1"
-      else if cell == "black"
-        black_pos = "1"
-      white = white_pos + white
-      black = black_pos + black
-  result =
-    white: white
-    black: black
 
 
+# Lets go!
+main = ()->
 
+  # Start by getting some game data
+  game_data = get_game_data()
 
-
-
-
-b = new Board(document.getElementById("board"), 8, 8)
-player = "black"
-b.register (row, col, event)->
-  console.log "Clicked! ->", row, col, event
-  if player == "white"
-    player = "black"
+  if game_data.board_size == 0
+    console.log "!! NEW GAME !!"
+    
   else
-    player = "white"
-  b.place(row, col, "stone set #{player}")
+    console.log "!! LOADING GAME !!"
+    # LOADING EXISTING GAME
 
-b.place(2, 2, "stone set white")
-b.place(5, 4, "stone set black")
-b.place(7, 1, "stone set black")
-b.place(5, 3, "stone set white")
+  console.log game_data
 
 
-
-# Protocal
-# Grab chunks of two? ie 00 = empty, 10 = white, 01 = black
-# board binary = board position in binary working left to right top to bottom. So a number 5 (101) would be a stone in the first position and third
-# white = board-binary (white positions)
-# black = board-binary (black positions)
-# turn = white/black
-# last = board-binday previous board position for last player?
-# size = boardsize
+main()
--- js/main.js
+++ js/main.js
@@ -1,98 +1,17 @@
 (function() {
-  var b, formBoard, genPositions, getPositions, parseBoard, parseURL, player;
+  var main;
 
-  parseURL = function() {
-    var elem, parse, query, url;
-    url = window.location.href;
-    elem = document.createElement("a");
-    elem.href = url;
-    query = decodeURI(elem.hash || elem.search);
-    parse = {};
-    query.replace(/([^?#=&]+)(=([^&]*))?/g, function($0, $1, $2, $3) {
-      return parse[$1] = $3;
-    });
-    return parse;
-  };
-
-  getPositions = function(num) {
-    return num.toString(2);
-  };
-
-  genPositions = function(pos) {
-    return parseInt(pos, 2);
-  };
-
-  formBoard = function(size, white, black) {
-    var black_pos, board, full_size, i, row, tmp, white_pos, _i, _ref;
-    full_size = Math.pow(size, 2);
-    board = Array();
-    row = Array();
-    tmp = Array();
-    for (i = _i = 1, _ref = full_size + 1; 1 <= _ref ? _i < _ref : _i > _ref; i = 1 <= _ref ? ++_i : --_i) {
-      white_pos = parseInt(white.charAt(white.length - i));
-      black_pos = parseInt(black.charAt(black.length - i));
-      if (white_pos && black_pos) {
-        throw "Invalid Board Configuration";
-      } else if (white_pos) {
-        row.push("white");
-      } else if (black_pos) {
-        row.push("black");
-      } else {
-        row.push("");
-      }
-      if (row.length === size) {
-        board.push(row);
-        row = Array();
-      }
-    }
-    return board;
-  };
-
-  parseBoard = function(board) {
-    var black, black_pos, cell, result, row, white, white_pos, _i, _j, _len, _len1;
-    white = "";
-    black = "";
-    for (_i = 0, _len = board.length; _i < _len; _i++) {
-      row = board[_i];
-      for (_j = 0, _len1 = row.length; _j < _len1; _j++) {
-        cell = row[_j];
-        white_pos = "0";
-        black_pos = "0";
-        if (cell === "white") {
-          white_pos = "1";
-        } else if (cell === "black") {
-          black_pos = "1";
-        }
-        white = white_pos + white;
-        black = black_pos + black;
-      }
-    }
-    return result = {
-      white: white,
-      black: black
-    };
-  };
-
-  b = new Board(document.getElementById("board"), 8, 8);
-
-  player = "black";
-
-  b.register(function(row, col, event) {
-    console.log("Clicked! ->", row, col, event);
-    if (player === "white") {
-      player = "black";
+  main = function() {
+    var game_data;
+    game_data = get_game_data();
+    if (game_data.board_size === 0) {
+      console.log("!! NEW GAME !!");
     } else {
-      player = "white";
+      console.log("!! LOADING GAME !!");
     }
-    return b.place(row, col, "stone set " + player);
-  });
+    return console.log(game_data);
+  };
 
-  b.place(2, 2, "stone set white");
-
-  b.place(5, 4, "stone set black");
-
-  b.place(7, 1, "stone set black");
-
-  b.place(5, 3, "stone set white");
+  main();
 
 }).call(this);
